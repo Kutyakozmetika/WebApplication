ca873108e194514de287362203adcb84b401333a
sz0barmar <sz0barmar@vasvari.org>
2022-04-16 10:38:58 +0200

    Tömb létrehozása scriptben.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,13 +6,20 @@
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: "Slider",
+  data() {
+    return {
+      images: [
+        "https://oroscafe.hu/wp-content/uploads/2021/03/210318_Kutyakozmetika.jpg",
+        "https://www.pet4you.hu/img/big/820.jpg",
+        "https://www.pet4you.hu/img/big/452_1.jpg",
+        "https://adatlistazo.hu/wp-content/uploads/2022/03/miert-fontos-a-kutyakozmetika-1.jpg"
+      ],
+      timer: null,
+      currentIndex: 0
+    };
+  },
+
 }
 </script>
