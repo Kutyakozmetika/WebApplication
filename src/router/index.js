e79c06bfa5b1bcd68df0b14776a8e1d1e6e1efd9
krisz501@sulid.hu <krisz501@sulid.hu>
2022-04-16 11:16:11 +0200

    Index.js módosítása, Router beállítás

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Szolgaltatasok from '../views/Szolgaltatasok.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 const routes = [
   {
@@ -8,13 +11,11 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/szolgaltatas',
+    name: 'Szolgaltatasok',
+    component: Szolgaltatasok
+  },
+
 ]
 
 const router = createRouter({
