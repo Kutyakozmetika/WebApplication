5978f4fc6adbdaf6054c3fdfbd8d54ed130ee6dd
krisz501@sulid.hu <krisz501@sulid.hu>
2022-04-16 11:07:03 +0200

    Script rész kész a szolgáltatásokban, get lekérés, szolgáltatások tömb létrehozása

---
+++ src/views/Szolgaltatasok.vue
@@ -0,0 +1,28 @@
+<template>
+  
+</template>
+
+<script>
+
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      szolgaltatasok: [],
+    };
+  },
+  mounted() {
+    axios.get("http://localhost:5000/api/szolgaltatas")
+      .then(response => {
+         this.szolgaltatasok = response.data
+      })
+      
+  }
+};
+
+</script>
+
+<style>
+
+</style>
