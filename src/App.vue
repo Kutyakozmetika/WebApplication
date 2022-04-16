dbc79a4a6f225a0e19f8a7997805d80191b31bb1
krisz501@sulid.hu <krisz501@sulid.hu>
2022-04-16 11:12:45 +0200

    app.vue kiegészítése és stílus módosítása

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="nav">
     <router-link to="/">Föoldal</router-link> |
-    <router-link to="/about">Letöltés</router-link> |
-    <router-link to="/about">Szolgáltatások</router-link> |
-    <router-link to="/about">Értékelés</router-link>
+    <router-link to="/szolgaltatas">Szolgáltatások</router-link> |
+    <router-link to="/arlista">Árlista</router-link> |
+    <router-link to="/kapcsolat">Kapcsolat</router-link>
+    <img id="logo" src="./assets/logo.png" >
   </div>
   <router-view/>
 </template>
@@ -19,14 +20,22 @@
 
 #nav {
   padding: 30px;
+  color: black;
+  font-size: 20px;
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: black;
+  font-size: 20px;
 }
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+#logo{
+  width: 100px;
+  height: 100px;
 }
 </style>
