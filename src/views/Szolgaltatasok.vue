bc0eed5487652a1227defd2c371feec8eb569ca2
krisz501@sulid.hu <krisz501@sulid.hu>
2022-04-16 11:08:09 +0200

    Template kész, kártyák létrehozása szolgáltatásokkal bootstrap használatával

--- src/views/Szolgaltatasok.vue
+++ src/views/Szolgaltatasok.vue
@@ -1,4 +1,39 @@
 <template>
+<div class="container">
+  <div class="row">
+    
+      <div class="card col-lg-12 col-md-6 col-sm-12 p-1" v-for="szolgaltatas in szolgaltatasok" :key="szolgaltatas.szolgaltatasNev">
+        
+        <div class="card-header">
+
+          <h4>{{szolgaltatas.szolgaltatasID}} </h4>
+          <h1>{{szolgaltatas.szolgaltatasNev}}</h1>
+         
+
+        </div>
+        
+        <div class="card-body" >
+
+          
+        <img id="img" src="../assets/dog.png"> 
+
+        </div>
+
+        <div class="card-footer">
+          
+          <h3>Időtartam: {{szolgaltatas.idotartam}} perc</h3>
+
+    
+        </div>
+
+      
+  
+   
+      </div>
+    
+  </div>
+</div>
+
   
 </template>
 
