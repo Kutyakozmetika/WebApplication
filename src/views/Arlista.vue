8d7e78f62a3c7fd908efd71fea4860c2beebe53e
András Dömösi <sz0domand@vasvari.org>
2022-04-17 14:49:37 +0200

    Arlista script kész

---
+++ src/views/Arlista.vue
@@ -0,0 +1,22 @@
+
+
+<script>
+
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      szolgaltatasok: [],
+    };
+  },
+  mounted() {
+    axios.get("http://localhost:5000/api/szolgaltatas%22")
+      .then(response => {
+         this.szolgaltatasok = response.data
+      })
+
+  }
+};
+
+</script>
