64a950b4058b48efe3834f0894ffe5d5c6a33d1e
András Dömösi <sz0domand@vasvari.org>
2022-04-17 15:02:19 +0200

    kapcsolat.vue kész

---
+++ src/views/Kapcsolat.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>Telefonszám: +36 30 123 4567</p>
+  <p>Email-cim: doggosessions@gmail.com</p>
+  <p>Cím: 6722 Szeged, Guttenberg utca 11</p>
+
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2758.8453776037513!2d20.141222215547554!3d46.253301579118244!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4744887400ddad33%3A0x898a2ad0addb8651!2sSzegedi%20SZC%20Vasv%C3%A1ri%20P%C3%A1l%20Gazdas%C3%A1gi%20%C3%A9s%20Informatikai%20Technikum!5e0!3m2!1shu!2shu!4v1650032860192!5m2!1shu!2shu" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+p{
+    font-size:20px;
+}
+</style>
