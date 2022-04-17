bf49f9a19dc3d0408fb924e7ed91f9fecd1b8116
András Dömösi <sz0domand@vasvari.org>
2022-04-17 15:04:37 +0200

    kapcsolat oldalra át lehet lépni

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Szolgaltatasok from '../views/Szolgaltatasok.vue'
 import Arlista from '../views/Arlista.vue'
+import Kapcsolat from '../views/Kapcsolat.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
@@ -20,8 +21,12 @@ const routes = [
     path: '/arlista',
     name: 'Arlista',
     component: Arlista
-
   },
+  {
+    path: '/kapcsolat',
+    name: 'Kapcsolat',
+    component: Kapcsolat
+  }
 
 ]
 
