309ff64f260199323d5d9d0e71c647dab5f6ec63
András Dömösi <sz0domand@vasvari.org>
2022-04-17 14:51:30 +0200

    Arlista template kész

--- src/views/Arlista.vue
+++ src/views/Arlista.vue
@@ -1,4 +1,20 @@
+<template>
 
+<table>
+<tr>
+    <th class="nev">Szolgáltatások</th>
+    <th class="ar">Ár</th>
+
+  </tr>
+
+</table>
+<table  v-for="szolgaltatas in szolgaltatasok" :key="szolgaltatas.szolgaltatasNev">
+
+<td class="nev">{{szolgaltatas.szolgaltatasNev}}</td>
+<td class="ar"> 4500 Ft</td>
+</table>
+
+</template>
 
 <script>
 
